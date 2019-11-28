d955d86782452846b1385844c8668ef287106041
IMP1 <IMP1@mail.com>
2019-11-28 12:21:44 +0000

    Add notes to main file

--- main.lua
+++ main.lua
@@ -1,3 +1,37 @@
+--[[
+
+world
+    broken into 2x2 metre cells
+
+track
+    
+
+train
+    moves to next signal / station
+
+switch
+    changes track connections
+
+signal
+    makes trains wait
+    can be manually triggered and automatically triggered
+
+schedule
+    assigns times to trigger events
+
+event
+    can be manually triggered and automatically triggered from schedule
+    can have conditional logic stuff
+    can set signals
+
+route
+    dictates where a train must be and when
+
+station
+    collection of platforms
+    
+--]]
+
 local train = require 'train'
 
 local trains = { 

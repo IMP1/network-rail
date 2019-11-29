5523013ec6fa8044adf134aafc2415194229c036
IMP1 <IMP1@mail.com>
2019-11-29 10:47:59 +0000

    Add second station

--- world_1.lua
+++ world_1.lua
@@ -1,17 +1,19 @@
 local world = {}
 
 world.world_string = [[
-     /__________\
-    /____________\
-___<______________>___
+     j__________l            j_____________l
+    j____________l          /               \
+___<______________>________<_________________>________
 ]]
 
 world.char_map = {
     ["_"] = "E_W",
     ["<"] = "NE_W",
     [">"] = "NW_E",
-    ["/"] = "E_SW",
-    ["\\"] = "W_SE",
+    ["j"] = "E_SW",
+    ["l"] = "W_SE",
+    ["/"] = "SW_NE",
+    ["\\"] = "NW_SE",
 }
 
 world.switches = {
@@ -22,7 +24,7 @@ world.switches = {
 }
 
 world.signals = {
-
+    {x = 22, y = 3, start = false}
 }
 
 world.stations = {
@@ -34,7 +36,16 @@ world.stations = {
                 tracks = { {8,1},{9,1},{10,1},{11,1},{12,1},{13,1} },
             },
         }
-    }
+    },
+    {
+        name = "Shipley", 
+        platforms = {
+            { 
+                name = "1", 
+                tracks = { {32,1},{33,1},{34,1},{35,1},{36,1},{37,1} },
+            },
+        }
+    },
 }
 
 return world

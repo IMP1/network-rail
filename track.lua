a17df6469a2c9e81122810ec52a482b48da9be1f
IMP1 <IMP1@mail.com>
2019-11-29 10:47:46 +0000

    Add colour as parameter for drawing tracks

--- track.lua
+++ track.lua
@@ -19,9 +19,9 @@ function track:next(dir)
     return track_directions[self.orientation][dir]
 end
 
-function track:draw(scale)
-    local size = track.SIZE * (scale or 1)
+function track:draw(scale, colour)
     local x, y = unpack(self.position)
+    local size = track.SIZE * (scale or 1)
     local points = {}
     for _, v in pairs(track_directions[self.orientation]) do
         local x, y = direction.to_offset(v)
@@ -30,9 +30,7 @@ function track:draw(scale)
     end
     table.insert(points, 3, 0)
     table.insert(points, 4, 0)
-    love.graphics.setColor(1, 0, 0, 0.5)
-    love.graphics.rectangle("line", (x-0.5) * size, (y-0.5) * size, size, size)
-    love.graphics.setColor(0, 0, 0)
+    love.graphics.setColor(colour or {0, 0, 0})
     love.graphics.push()
     love.graphics.translate(x * size, y * size)
     love.graphics.line(points)

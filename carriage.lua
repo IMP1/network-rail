7c2b72456a4baa2f79bdd92b2d5a62146eb5ffb1
IMP1 <IMP1@mail.com>
2019-12-03 15:43:16 +0000

    Change units to pixels

--- carriage.lua
+++ carriage.lua
@@ -12,10 +12,10 @@ function carriage.new(options)
     setmetatable(self, carriage)
 
     self.colour    = options.colour    or {0.1, 0.1, 0.1}
-    self.position  = options.position  or {0, 0}
+    self.position  = options.position  or {0, 0} -- pixels
     self.direction = options.direction or 0  -- radians
-    self.length    = options.length    or 20 -- metres
-    self.width     = options.width     or 3  -- metres
+    self.length    = options.length    or 20 -- pixels
+    self.width     = options.width     or 3  -- pixels
     self.shape     = options.shape     or carriage.shapes.SQUARE
 
     return self

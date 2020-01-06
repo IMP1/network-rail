4223b760bf6e7759911b4dea76fd3564aa9be510
IMP1 <IMP1@mail.com>
2020-01-06 15:30:21 +0000

    Add time library for parsing times; Add time to clocks

--- clock.lua
+++ clock.lua
@@ -1,3 +1,5 @@
+local time = require 'time'
+
 local clock = {}
 clock.__index = clock
 
@@ -5,36 +7,21 @@ function clock.new(options)
     local self = {}
     setmetatable(self, clock)
 
-    self.seconds = options.seconds or 0
-    self.minutes = options.minutes or 0
-    self.hours   = options.hours   or 0
+    self.time = options.time or time.new()
 
     return self
 end
 
 function clock:update(dt)
-    self.seconds = self.seconds + dt
-    while self.seconds >= 60 do
-        self.seconds = self.seconds - 60
-        self.minutes = self.minutes + 1
-    end
-    while self.minutes >= 60 do
-        self.minutes = self.minutes - 60
-        self.hours = self.hours + 1
-    end
-    while self.hours >= 24 do
-        self.hours = self.hours - 24
+    local days_on
+    self.time, days_on = self.time.advance("second", dt)
+    if days_on > 0 then
+        -- TODO: new day!
     end
 end
 
 function clock:__tostring(format)
-    local str = format or "%H:%M:%S"
-    str = str:gsub("%%S", string.format("%02d", math.floor(self.seconds)))
-    str = str:gsub("%%M", string.format("%02d", math.floor(self.minutes)))
-    str = str:gsub("%%H", string.format("%02d", math.floor(self.hours)))
-    return str
-                 
-                 
+    return tostring(self.time, format)
 end
 
 return clock
---
+++ time.lua
@@ -0,0 +1,72 @@
+local time = {}
+time.__index = time
+
+function time.parse(text)
+    local hour, minute, second 
+    hour, minute, second = text:match("(%d%d):(%d%d):(%d%d)")
+    if hour == nil then
+        hour, minute = text:match("(%d%d):(%d%d)")
+        second = 0
+    end
+    return time.new(hour, minute, second)
+end
+
+function time.new(hour, minute, second)
+    local self = {}
+    setmetatable(self, time)
+
+    self.hour   = hour   or 0
+    self.minute = minute or 0
+    self.second = second or 0
+
+    return self
+end
+
+function time:advance(unit, amount)
+    local hour, minute, second = self.hour, self.minute, self.second
+    if unit == "hour" or unit == "H" then
+        local hour_change = math.floor(amount)
+        hour = hour + hour_change        
+        amount = amount - hour_change
+        if amount > 0 then
+            local minute_change = math.floor(60 * amount)
+            minute = minute + minute_change
+        end
+    elseif unit == "minute" or unit == "M" then
+        local minute_change = math.floor(amount)
+        minute = minute + minute_change
+        amount = amount - minute_change
+        if amount > 0 then
+            local second_change = math.floor(60 * amount)
+            second = second + second_change
+        end
+    elseif unit == "second" or unit == "S" then
+        second = second + amount
+    else
+        error("Unrecognised unit '" .. unit .. "'. Should be hour, minute, or second")
+    end
+    local days_changed = 0
+    while second >= 60 do
+        second = second - 60
+        minute = minute + 1
+    end
+    while minute >= 60 do
+        minute = minute - 60
+        hour = hour + 1
+    end
+    while hour >= 24 do
+        hour = hour - 24
+        days_changed = days_changed + 1
+    end
+    return time.new(hour, minute, second), days_changed
+end
+
+function time:__tostring(format)
+    local str = format or "%H:%M:%S"
+    str = str:gsub("%%H", string.format("%02d", math.floor(self.hour)))
+    str = str:gsub("%%M", string.format("%02d", math.floor(self.minute)))
+    str = str:gsub("%%S", string.format("%02d", math.floor(self.second)))
+    return str
+end
+
+return time

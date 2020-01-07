b437c4571ddc0c85a8493bba139c3137e1a1ff52
IMP1 <IMP1@mail.com>
2020-01-07 12:02:17 +0000

    Add helper function; Add arrival times to route

--- route.lua
+++ route.lua
@@ -21,18 +21,47 @@ function route:step()
     end
 end
 
+function route:arriveAt(station, time)
+    if self:nextStation() == station then
+        self:nextStop().arrival_time = time
+    else
+        local skipped_index = 0
+        for i = self.current_position, #self.stops do
+            if self.stops[i].station == station then
+                skipped_index = i
+                break
+            end
+        end
+        if skipped_index > 0 then
+            for i = self.current_position, skipped_index - 1 do
+                self.stops[i].arrival_time = nil
+            end
+            self.stops[skipped_index].arrival_time = time
+        else
+            -- TODO: train was not due at this station. Just ignore it?
+        end
+    end
+end
+
+function route:nextStop()
+    if self.finished then 
+        return nil
+    end
+    return self.stops[self.current_position]
+end
+
 function route:nextStation()
     if self.finished then 
         return nil
     end
-    self.stops[self.current_position].station
+    return self:nextStop().station
 end
 
 function route:nextArrivalTime()
     if self.finished then 
         return nil
     end
-    self.stops[self.current_position].time
+    return self:nextStop().time
 end
 
 return route

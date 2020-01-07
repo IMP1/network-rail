1796a7bd5dc1eab5f620b9084d9bd64a21629c16
IMP1 <IMP1@mail.com>
2020-01-07 12:01:49 +0000

    Restructe pallete so it can self-refer

--- pallete_default.lua
+++ pallete_default.lua
@@ -1,10 +1,20 @@
-local pallete = {
-    BLACK = {0, 0, 0},
-    WHITE = {1, 1, 1},
-    STOP   = {0.7, 0, 0},
-    GO = {0, 0.7, 0},
-    SELECTABLE = {0, 0, 1, 0.5},
-    SELECTION = {1, 1, 0, 1},
-}
+local pallete = {}
+    
+pallete.BLACK = {0, 0, 0}
+pallete.WHITE = {1, 1, 1}
+
+pallete.STOP = {0.7, 0, 0}
+pallete.WARN = {0.7, 0.5, 0}
+pallete.GO   = {0, 0.7, 0}
+
+pallete.SELECTABLE = {1, 1, 1, 0.5}
+pallete.SELECTION  = {0, 1, 0.5, 1}
+
+pallete.LATE    = pallete.STOP
+pallete.MISSED  = pallete.WARN
+pallete.ON_TIME = pallete.BLACK
+
+pallete.CANCELLED = pallete.STOP
+pallete.DELAYED   = pallete.WARN
 
 return pallete
